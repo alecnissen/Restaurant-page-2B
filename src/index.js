4b5afc9e36988a8d590f1cdb2301b7c0448f9ff5
alecnissen <alecnissen@yahoo.com>
2023-02-28 15:40:50 -0500

    debugging, working on having initial page load module, run it's code and print to the DOM

--- src/index.js
+++ src/index.js
@@ -6,10 +6,9 @@
 
 // import { dumbFunction } from "./initialPageLoad";
 
-// console.log(dumbFunction()); 
 
 import './style.css';
 
-import initialPageLoad from "./initialPageLoad"; 
+import { initialPageLoad } from './initialPageLoad';
 
-console.log(initialPageLoad());
+initialPageLoad();
--- src/initialPageLoad.js
+++ src/initialPageLoad.js
@@ -1,28 +1,97 @@
-//   export default function initialPageLoad () { 
-//     const headerContent = () => { 
-//     let container = document.getElementById('content');
-//     console.log(container);
-//     }
-//   } 
 
-  
-//   initialPageLoad(); 
+ export function initialPageLoad () { 
+    const headerContent = () => { 
+    
+    const contentDivContainer = document.getElementById('content'); 
 
-export default function initialPageLoad () { 
-    const contentDiv = document.getElementById('content');
+    console.log(contentDivContainer);
+    
     const headerContent = document.createElement('div');
-    // const headerContainer = document.createElement('container');
 
     headerContent.classList.add('header-content-container');
 
     headerContent.innerHTML = "Alec's House of Burgers <br> The Best In The City!";
 
-    // headerContainer.append(headerContent);
+    contentDivContainer.append(headerContent);
 
-    contentDiv.append(headerContent);
+    document.body.append(contentDivContainer);
 
-    document.body.append(contentDiv);
-
-    // return contentDiv;
+    } 
+    
+    return { 
+        headerContent,
+    }
 } 
+
+// this was able to print the text to the screen, 
+
+// the only difference is that there is no inner function, 
+
+
+// export function initialPageLoad () { 
+//     const contentDiv = document.getElementById('content');
+//     const headerContent = document.createElement('div');
+//     // const headerContainer = document.createElement('container');
+
+//     headerContent.classList.add('header-content-container');
+
+//     headerContent.innerHTML = "Alec's House of Burgers <br> The Best In The City!";
+
+//     contentDiv.append(headerContent);
+
+//     document.body.append(contentDiv);
+
+//     // return headerContent;
+// }  
+
+
+
+// why does this function work when I don't have a method attached to it 
+// I need each section within their own function 
+// so it can be called, and it's data can be private. 
+
+// remember 3 sections, header, main, footer, each of those sections 
+// each section uses a div, then adds content to the div, then 
+// appends the div to the main content container. 
+
+// go back to previous function, get output, then 
+// then refactor to use modules 
+
+// I was able to get output using just one function, 
+// but I thought each section was supposed to be it's own function
+// as well??? 
+
+// can I get output from just the one function ? 
+
+// it works when I dont headerContent within a function, 
+// but I thought that's the point of modules, 
+// keeping certain functions tucked away and 
+// keeping their data private. 
+
+// I am having trouble with rest page, 
+
+// I am trying to write my inital page load function 
+
+// within it's own file and module, 
+
+// I am trying to append a div with some simple text 
+
+// to a container 
+
+//  I am exporting it to another page, which then
+// calls the function to run, 
+
+// right now I just one error stating cannot apply style 
+
+//from dist/style.css, css is in src folder, 
+
+// also I noticed that when I remove the inner function, 
+
+// from inital page load, it works fine 
+
+// which confused me because I thought each section should be 
+
+// in it's own function, to keep data private, 
+
+// I'm confused on why this not working. 
 
