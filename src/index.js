2478b18d9ba7186c077575bd0b3fc15dcd3f6e54
alecnissen <alecnissen@yahoo.com>
2023-02-27 19:41:30 -0500

    able to render basic title and subtitle text to the screen, installed style loader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 // import './initialPageLoad';
 // import { initialPageLoad } from './initialPageLoad';
-import initialPageLoad from './initialPageLoad';
+// import initialPageLoad from './initialPageLoad';
 
-console.log(initalPageLoad());
+// console.log(initialPageLoad.headerContent()); 
+
+// import { dumbFunction } from "./initialPageLoad";
+
+// console.log(dumbFunction()); 
+
+import './style.css';
+
+import initialPageLoad from "./initialPageLoad"; 
+
+console.log(initialPageLoad());
--- src/initialPageLoad.js
+++ src/initialPageLoad.js
@@ -1,12 +1,28 @@
-  export default function initialPageLoad () { 
-    const headerContent = () => { 
-    let container = document.getElementById('content');
-    console.log(container);
-    }
-    return { 
-        headerContent,
-    }
-  } 
+//   export default function initialPageLoad () { 
+//     const headerContent = () => { 
+//     let container = document.getElementById('content');
+//     console.log(container);
+//     }
+//   } 
 
   
-//   initialPageLoad.headerContent();
+//   initialPageLoad(); 
+
+export default function initialPageLoad () { 
+    const contentDiv = document.getElementById('content');
+    const headerContent = document.createElement('div');
+    // const headerContainer = document.createElement('container');
+
+    headerContent.classList.add('header-content-container');
+
+    headerContent.innerHTML = "Alec's House of Burgers <br> The Best In The City!";
+
+    // headerContainer.append(headerContent);
+
+    contentDiv.append(headerContent);
+
+    document.body.append(contentDiv);
+
+    // return contentDiv;
+} 
+
