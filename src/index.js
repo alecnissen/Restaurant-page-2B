341464cd9fde8c9edcaa06b75da0f3d98b424118
alecnissen <alecnissen@yahoo.com>
2023-03-03 21:32:14 -0500

    working on tab switching logic, trying to remove previous contents and replace new contents

--- src/index.js
+++ src/index.js
@@ -20,4 +20,62 @@ initialPageLoad().headerContent();
 
 initialPageLoad().mainContent();
 
-initialPageLoad().footerContent();
+initialPageLoad().footerContent();  
+
+let moduleVariable 
+
+let headerHomeBtn = document.querySelector('.home-btn');
+
+let headerMenuBtn = document.querySelector('.menu-btn'); 
+
+let headerContactBtn = document.querySelector('.contact-btn');
+
+console.log(headerHomeBtn);
+
+console.log(headerMenuBtn);
+
+console.log(headerContactBtn);
+
+headerHomeBtn.addEventListener('click', () => {
+    moduleVariable = 'Home';
+    determinePage();
+    return
+}) 
+
+headerMenuBtn.addEventListener('click', () => { 
+    moduleVariable = 'Menu';
+    determinePage();
+    return
+}) 
+
+headerContactBtn.addEventListener('click', () => { 
+    moduleVariable = 'Contact';
+    determinePage();
+    return
+})
+
+// remove previous content/styles then render new 
+
+function determinePage () { 
+    let mainContentDivContainer = document.getElementById('content'); 
+    console.log(mainContentDivContainer);
+    if (moduleVariable === 'Home') { 
+        mainContentDivContainer.remove();
+
+        initialPageLoad().headerContent(); 
+        initialPageLoad().mainContent();
+
+        initialPageLoad().footerContent();  
+    } else if (moduleVariable === 'Menu') { 
+        mainContentDivContainer.remove();
+        initialPageLoad().headerContent(); 
+        initialPageLoad().footerContent();  
+    } else if (moduleVariable === 'Contact') { 
+        mainContentDivContainer.remove();
+
+        initialPageLoad().headerContent(); 
+        initialPageLoad().footerContent(); 
+    }
+} 
+
+determinePage();
