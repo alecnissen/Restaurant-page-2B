2c028168b673fbe6385a752b9022efd8d1fa47ed
alecnissen <alecnissen@yahoo.com>
2023-03-04 22:37:44 -0500

    checkpoint, working on using replaceChild to update main content section

--- src/index.js
+++ src/index.js
@@ -15,7 +15,11 @@ mainContent();
 
 footerContent(); 
 
-let contentDivContainer = document.getElementById('content');
+let contentDivContainer = document.getElementById('content'); 
+
+let containerChildren = contentDivContainer.children; 
+
+console.log(containerChildren);
 
 // variable to determine which module the user clicks, if menu is pressed, 
 // render menu content, if contact is pressed, render contact info 
@@ -59,13 +63,109 @@ function determinePage () {
         mainContent();
         footerContent();  
     } else if (moduleVariable === 'Menu') { 
-        headerContent(); 
-        menuContent();
-        footerContent();  
+        // headerContent(); 
+        console.log('hey');
+        contentDivContainer.replaceChild(menuContent(), containerChildren[1]);
+        // menuContent();
+        // footerContent();  
     } else if (moduleVariable === 'Contact') { 
         headerContent(); 
         footerContent(); 
     }
 } 
 
-determinePage();
+determinePage(); 
+
+
+
+
+// codepen example 
+// const container = document.getElementById("container");
+// const changeCenter = document.getElementById("change");
+
+// // create a function to dupliate the header, footer, container
+
+// const header = (() => {
+//   const createHeader = () => {
+//     let header = document.createElement("header");
+//     let h1 = document.createElement("h1");
+//     h1.textContent = "Header";
+//     header.appendChild(h1);
+//     container.appendChild(header);
+//   };
+//   return { createHeader };
+// })();
+
+// // add center and footer and call on load page
+
+// const center = () => { 
+//   let centerDiv = document.createElement('div'); 
+//   let h1 = document.createElement('h1'); 
+//   h1.textContent = 'Center'; 
+//   centerDiv.appendChild(h1);
+//   container.appendChild(centerDiv);
+// } 
+
+// const footer
+
+
+
+// const loadPage = (() => {
+//   header.createHeader()
+//   center();
+// })();
+
+// // on button click change center content 
+
+
+// const container = document.getElementById("container");
+// const changeCenter = document.getElementById("change");
+
+// // create a function to dupliate the header, footer, container
+
+// const header = (() => {
+//   const createHeader = () => {
+//     let header = document.createElement("header");
+//     let h1 = document.createElement("h1");
+//     h1.textContent = "Header";
+//     header.appendChild(h1);
+//     container.appendChild(header);
+//   };
+//   return { createHeader };
+// })();
+
+// // add center and footer and call on load page
+
+// const center = () => { 
+//   let centerDiv = document.createElement('div'); 
+//   let h1 = document.createElement('h1'); 
+//   h1.textContent = 'Center'; 
+//   centerDiv.appendChild(h1);
+//   container.appendChild(centerDiv);
+// } 
+
+// const footer = () => { 
+//  let footerDiv = document.createElement('div'); 
+//  let h1 = document.createElement('h1');
+//  h1.textContent = 'Footer';
+//  footerDiv.appendChild(h1);
+//  container.appendChild(footerDiv);
+  
+// }
+
+
+
+// const loadPage = (() => {
+//   header.createHeader()
+//   center();
+//   footer();
+// })();
+
+// // on button click change center content
+
+// changeCenter.addEventListener('click', () => { 
+//  // remove the center element, tried using .remove() 
+//   center().remove();
+  
+  
+// })
